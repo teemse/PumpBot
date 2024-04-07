a4a83d9c933899f4afba196fcad2e785930a4442
teemse <chernikov.artem2000@gmail.com>
2024-04-08 01:20:53 +0300

    2

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 from binance.um_futures import UMFutures
 from tradingview_ta import TA_Handler, Interval, Exchange
 
-
 INTERVAL = Interval.INTERVAL_15_MINUTES
 TELEGRAM_TOKEN = ''
 TELEGRAM_CHANNEL = ''
@@ -20,7 +19,6 @@ def get_data(symbol):
     activiti['SYMBOL'] = symbol
     return activiti
 
-
 def get_symbols():
     tickers = client.mark_price()
     symbols = []
@@ -32,29 +30,28 @@ def get_symbols():
 def send_massage(text):
     res = requests.get('https://api.telegram.org/bot{}/sendMassage'.format(TELEGRAM_TOKEN), params=dict(
     chat_id=TELEGRAM_CHANNEL, text=text))
-    
+
 
 symbols = get_symbols()
 longs = []
 shorts= []
 
-
 def first_data():
     print('Поиск начальных данных')
     send_massage('Поиск начальных данных')
     for i in symbols:
-        try:
-            data = get_data(i)
-            #print(data)
-            if (data['RECOMMENDATION'] == 'STRONG_BUY'):
-                longs.append(data['SYMBOL'])
-                #print(data['SYMBOL'], 'Buy')
+            try:
+                data = get_data(i)
+                #print(data)
+                if (data['RECOMMENDATION'] == 'STRONG_BUY'):
+                    longs.append(data['SYMBOL'])
+                    #print(data['SYMBOL'], 'Buy')
             
-            if (data['RECOMMENDATION'] == 'STRONG_SELL'):
-                shorts.append(data['SYMBOL'])
-            time.sleep(0.01)
-        except:
-            pass
+                if (data['RECOMMENDATION'] == 'STRONG_SELL'):
+                    shorts.append(data['SYMBOL'])
+                time.sleep(0.01)
+            except:
+                pass
     print('На лонг:')
     print(longs)
     print('На шорт:')
@@ -73,7 +70,7 @@ while True:
             #print(data)
             if (data['RECOMMENDATION'] == 'STRONG_BUY' and (data['SUMBOL'] not in longs)):
                 print(data['SUMBOL'], 'Buy')
-                text = data['SUMBOL'] + 'Buy'
+                text = data['SUMBOL'] + 'BUY'
                 send_massage(text)
                 longs.append(data['SUMBOL'])
 
@@ -83,7 +80,7 @@ while True:
                 print(data['SUMBOL'], 'Sell')
                 text = data['SUMBOL'] + 'SELL'
                 send_massage(text)
-                longs.append(data['SUMBOL'])
+                shorts.append(data['SUMBOL'])
             time.sleep(0.1)
         except:
             pass
