b466a1f1998867e401cc42e39a105573053a42a4
teemse <31262294+teemse@users.noreply.github.com>
2024-04-08 20:08:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,19 @@
+# импорты
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+
 # Объект бота
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+# Для записей с типом Secret* необходимо 
+# вызывать метод get_secret_value(), 
+# чтобы получить настоящее содержимое вместо '*******'
+bot = Bot(token=config.bot_token.get_secret_value())
+
 # Диспетчер
 dp = Dispatcher()
 
