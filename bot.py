9e30cd806a70c8a9f8e1f6dbc85b61637508200b
teemse <chernikov.artem2000@gmail.com>
2024-04-09 01:31:59 +0300

    work

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config_reader import config
+from aiogram import F
+from main import first_data, longs, shorts
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -22,16 +24,31 @@ dp = Dispatcher()
 async def cmd_start(message: types.Message):
     kb = [
         [
-            types.KeyboardButton(text="С пюрешкой"),
-            types.KeyboardButton(text="Без пюрешки")
+            types.KeyboardButton(text="Binance"),
+            types.KeyboardButton(text="Forex")
         ],
     ]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
         resize_keyboard=True,
-        input_field_placeholder="Выберите способ подачи"
+        input_field_placeholder="Выберите биржу"
     )
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    await message.answer("Кого ебать будем?", reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "forex")
+async def without_puree(message: types.Message):
+    await message.reply("Охуел что ли")
+    await message.reply("Пока ебнутый", reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message(F.text.lower() == "binance")
+async def with_puree(message: types.Message):
+    await message.reply("Ну погнали!")
+    await message.answer("Ожидай отец, скамлю мамонтов...", reply_markup=types.ReplyKeyboardRemove())
+    first_data()
+    await message.answer("На эти не смотри нахуй")
+    await message.answer( ','.join(map(str, longs)))
+    await message.answer(','.join(map(str, shorts)))
+    print('Погнали')
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
--- main.py
+++ main.py
@@ -1,13 +1,80 @@
+# импорты
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from config_reader import config
+from aiogram import F
 import time
 import requests
 from binance.um_futures import UMFutures
 from tradingview_ta import TA_Handler, Interval, Exchange
 
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота
+# Для записей с типом Secret* необходимо 
+# вызывать метод get_secret_value(), 
+# чтобы получить настоящее содержимое вместо '*******'
+bot = Bot(token=config.bot_token.get_secret_value())
+
+# Диспетчер
+dp = Dispatcher()
+
+INTER=[Interval.INTERVAL_1_MINUTE, 
+       Interval.INTERVAL_5_MINUTES,
+       Interval.INTERVAL_15_MINUTES,
+       Interval.INTERVAL_30_MINUTES,
+       Interval.INTERVAL_1_HOUR,
+       Interval.INTERVAL_2_HOURS,
+       Interval.INTERVAL_4_HOURS,
+       Interval.INTERVAL_1_DAY,
+       Interval.INTERVAL_1_WEEK,
+       Interval.INTERVAL_1_MONTH]
+
 INTERVAL = Interval.INTERVAL_15_MINUTES
-TELEGRAM_TOKEN = ''
-TELEGRAM_CHANNEL = ''
 
 client = UMFutures()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Binance"),
+            types.KeyboardButton(text="Forex")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите биржу"
+    )
+    await message.answer("Кого ебать будем?", reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "forex")
+async def forex(message: types.Message):
+    await message.reply("Охуел что ли")
+    await message.answer("Пока ебнутый", reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message(F.text.lower() == "binance")
+async def binance(message: types.Message):
+    kb = [
+        [
+            types.KeyboardButton(text="Понял"),
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Быстрее"
+    )
+    await message.answer("Ожидай отец, скамлю мамонтов...", reply_markup=types.ReplyKeyboardRemove())
+    first_data()
+    await message.answer("Эти не берем", reply_markup=keyboard)
+    await message.answer( ','.join(map(str, longs)))
+    await message.answer(','.join(map(str, shorts)))
 
 def get_data(symbol):
     output = TA_Handler(symbol=symbol,
@@ -27,11 +94,6 @@ def get_symbols():
         symbols.append(ticker)
     return symbols
 
-def send_massage(text):
-    res = requests.get('https://api.telegram.org/bot{}/sendMassage'.format(TELEGRAM_TOKEN), params=dict(
-    chat_id=TELEGRAM_CHANNEL, text=text))
-
-
 symbols = get_symbols()
 longs = []
 shorts= []
@@ -39,11 +101,10 @@ shorts= []
 def first_data():
     print('Сбор начальных данных...')
     print('\n'.join(symbols))
-    send_massage('Сбор начальных данных...')
     for i in symbols:
             try:
                 data = get_data(i)
-                # print(data)
+                print(data)
                 if (data['RECOMMENDATION'] == 'STRONG_BUY'):
                     longs.append(data['SYMBOL'])
                     # print(data['SYMBOL'], 'Buy')
@@ -59,29 +120,36 @@ def first_data():
     print(shorts)
     return longs, shorts
 
-first_data()
-print('Погнали')
-send_massage('Погнали')
+@dp.message(F.text.lower() == "понял")
+async def basic(message: types.Message):
+    while True:
+        print('_______________________Новый цикл______________________')
+        await message.answer("Ну погнали!")
+        await message.answer("_______________Новый цикл______________!", reply_markup=types.ReplyKeyboardRemove())
 
-while True:
-    print('_______________________Новый цикл______________________')
-    for i in symbols:
-        try:
-            data = get_data(i)
-            # print(data)
-            if (data['RECOMMENDATION'] == 'STRONG_BUY' and (data['SYMBOL'] not in longs)):
-                print(data['SYMBOL'], 'Берем')
-                text = data['SYMBOL'] + 'BUY'
-                send_massage(text)
-                longs.append(data['SYMBOL'])
+        for i in symbols:
+            try:
+                data = get_data(i)
+                print(data)
+                if (data['RECOMMENDATION'] == 'STRONG_BUY' and (data['SYMBOL'] not in longs)):
+                    print(data['SYMBOL'], ' Брать надо')
+                    text = data['SYMBOL'] + ' Брать надо'
+                    await message.answer(text)
+                    longs.append(data['SYMBOL'])
 
 
 
-            if (data['RECOMMENDATION'] == 'STRONG_SELL' and (data['SYMBOL'] not in shorts)):
-                print(data['SYMBOL'], 'Сливаем')
-                text = data['SYMBOL'] + 'SELL'
-                send_massage(text)
-                shorts.append(data['SYMBOL'])
-            time.sleep(0.1)
-        except:
-            pass
+                if (data['RECOMMENDATION'] == 'STRONG_SELL' and (data['SYMBOL'] not in shorts)):
+                    print(data['SYMBOL'], ' Продаем нахуй')
+                    text = data['SYMBOL'] + ' Продаем нахуй'
+                    await message.answer(text)
+                    shorts.append(data['SYMBOL'])
+                time.sleep(0.1)
+            except:
+                pass
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
